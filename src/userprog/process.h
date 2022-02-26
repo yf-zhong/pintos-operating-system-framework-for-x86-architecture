bfed3dce9633dc5f7accf65bb9dc4d20759594ae
Yifan Zhong <yzhong5@berkeley.edu>
2022-02-26 14:23:25 -0800

    Add file_descriptor structure and decriptor list in pcb.

--- src/userprog/process.h
+++ src/userprog/process.h
@@ -46,6 +46,18 @@ struct process {
   struct lock c_lock;
   struct list children;
   struct child* curr_as_child;
+  int cur_fd;                 /* The fd number assigned to new file */
+  struct lock file_sys_lock;  /* Lock for the file system operation */
+  struct list file_descriptor_table; /* All the files opened in current process */
+};
+
+/* One element in the file descriptor table */
+struct file_descriptor {
+   int fd;                   /* File descriptor */
+   struct file *file;        /* File descriptor */
+   int ref_cnt;              /* Number of processes currently using the file */
+   struct lock ref_cnt_lock; /* Protect reference count per file */
+   struct list_elem elem;
 };
 
 // NEW_c has to come first so that 
