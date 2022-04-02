8573681b944c5bb17ac86fdd47e854c956d67e79
Kin Long Lo <kinlonglo98065@gmail.com>
2022-04-02 07:22:49 +0000

    fix deadlock caused by process_lock

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -230,7 +230,6 @@ tid_t thread_create(const char* name, int priority, thread_func* function, void*
 void thread_block(void) {
   ASSERT(!intr_context());
   ASSERT(intr_get_level() == INTR_OFF);
-
   thread_current()->status = THREAD_BLOCKED;
   schedule();
 }
